a68ba3cfcdb91d9454eb613c1928e02ded2054b1
unknown <g00373427@gmit.ie>
2022-05-02 15:24:02 +0100

    Fruit Movement

---
+++ GestureProjectKinect/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    /* Private Variables */
+    private Vector3 target = new Vector3(0.0f, 1.0f, 0.0f);
+    /* Public Variables */
+    public int moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+
+
+    private void MoveEnemy() {
+        var step =  moveSpeed * Time.deltaTime; // calculate distance to move
+        //transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+    }
+}
